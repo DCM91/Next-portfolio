c39a069743ef8e5f45cf6080e21027788981c742
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2023-05-08 17:22:33 +0200

    todos links navbar y pagiinas hechas para empezar

--- portfoliodcm91/components/navbar.js
+++ portfoliodcm91/components/navbar.js
@@ -1,5 +1,6 @@
 import en from "@/languages/en"
 import es from "@/languages/es"
+import Link from "next/link";
 import { useRouter } from 'next/router';
 
 export const Navbar = ({theme}) => {
@@ -14,23 +15,14 @@ export const Navbar = ({theme}) => {
   return (
       <div data-theme={theme} className="navbar flex flex-wrap w-full bg-base-100">
         <div className="flex-1 ">
-          <a className="btn btn-ghost normal-case text-3xl">Daniel Castro Martín</a>
+          <Link href={"/"} className="btn btn-ghost normal-case text-3xl">Daniel Castro Martín</Link>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal text-error">
-            <li><a>Home</a></li>
-            <li tabIndex={0}>
-              <a>
-                Info
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-              </a>
-              <ul className="p-2 bg-base-100">
-                <li><a>{t.navbar.about}</a></li>
-                <li><a>{t.navbar.SKILLS}</a></li>
-                <li><a>{t.navbar.works}</a></li>
-              </ul>
-            </li>
-            <li><a>{t.navbar.contact}</a></li>
+            <li><Link href={"/about"}>{t.navbar.about}</Link></li>
+            <li><Link href={"/skills"}>{t.navbar.SKILLS}</Link></li>
+            <li><Link href={"/works"}>{t.navbar.works}</Link></li>
+            <li><Link href={"/contact"}>{t.navbar.contact}</Link></li>
           </ul>
       </div>
     </div>
