a8de2ec13cccc82cd15337e005625fa2aa16856e
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2023-05-29 20:51:23 +0200

    mejorando cositas

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ import es from "@/languages/es"
 import { MdWavingHand } from "react-icons/md";
 import Link from "next/link";
 import Image from "next/image";
+import { Footer } from "@/components/Footer";
+import Head from "next/head";
+import { Selector } from "@/components/selector";
 
 export default function Home() {
   const router = useRouter()
@@ -17,19 +20,25 @@ export default function Home() {
 
 
   return (
-     <Layout title="Dcm91Portfolio - Home">
-      <div className="grid w-9/12">
+      <div className="grid w-screen h-screen bg-black">
+          <Head>
+            <title>Dcm91Portfolio - Home</title>
+            <meta name="description" content="Dcm91 profesional Portfolio" />
+            
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-        <div className="h-10 w-100  p-4">
-            <h1 className="text-2xl flex">{t.home.title} &nbsp; <MdWavingHand/></h1>
+        <div className="w-100  p-4">
+            <h1 className="text-2xl  text-primary flex">{t.home.title} &nbsp; <MdWavingHand/></h1>
         </div>
-        <div className="flex flex-wrap mt-5"> 
-          <div className="border-yellow-50 border-4 flex-1  p-3">
+        <Selector/>
+        <div className="flex flex-wrap"> 
+          <div className="border-yellow-50 border-4 flex-1 w-2/3  p-3">
             <Image src={"/assets/devIAPortfolio.jpg"} width={250} height={250} alt="kakaka" />
           </div>
 
 
-          <div className="flex-2">
+          <div className="flex-2 w-1/3">
             <ul className="grid text-error">
               <li className="p-2"><Link href={"/about"}>{t.navbar.about}</Link></li>
               <li className="p-2"><Link href={"/skills"}>{t.navbar.SKILLS}</Link></li>
@@ -38,7 +47,7 @@ export default function Home() {
             </ul>  
         </div>
         </div>
+        <Footer />
       </div>   
-      </Layout>
   )
 }
