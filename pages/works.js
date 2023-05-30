ac3716c29df48afe45712d9c334dfb11aadddf04
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2023-05-30 19:59:23 +0200

    works

--- pages/works.js
+++ pages/works.js
@@ -4,18 +4,36 @@ import Image from "next/image"
 export default function Works() {
   return (
     <Layout title="Dcm91Portfolio - Works">
-      <div className="flex h-fit px-10 pb-4">
-        <div className="border-solid border-2 border-orange-900 flex">
+      <div className="flex flex-wrap h-fit px-10 pb-1">
+        <div className="border-solid border-2 border-orange-900 m-1">
           <Image src={"/projects/skuadlack.png"} width={300} height={300} alt="kakaka" />
         </div>
-        <div className="border-solid border-2 border-orange-900 flex">
+        <div className="border-solid border-2 border-orange-900 m-1">
           <Image src={"/projects/blockbuster.png"} width={300} height={300} alt="kakaka" />
         </div>
-        <div className="border-solid border-2 border-orange-900 flex">
+        <div className="border-solid border-2 border-orange-900 m-1">
           <Image src={"/projects/pokedex.png"} width={300} height={300} alt="kakaka" />
         </div>
-        <div className="border-solid border-2 border-orange-900 flex">
-          <Image src={"/projects/calculator.png"} width={300} height={300} alt="kakaka" />
+        <div className="border-solid border-2 border-orange-900 m-1 flex">
+          <Image src={"/projects/internacionalizacion.png"} width={300} height={300} alt="kakaka" />
+        </div>
+
+
+
+
+
+
+        <div className="border-solid border-2 border-orange-900 m-1">
+          <Image src={"/projects/skuadlack.png"} width={300} height={300} alt="kakaka" />
+        </div>
+        <div className="border-solid border-2 border-orange-900 m-1">
+          <Image src={"/projects/blockbuster.png"} width={300} height={300} alt="kakaka" />
+        </div>
+        <div className="border-solid border-2 border-orange-900 m-1">
+          <Image src={"/projects/pokedex.png"} width={300} height={300} alt="kakaka" />
+        </div>
+        <div className="border-solid border-2 border-orange-900 m-1 flex">
+          <Image src={"/projects/internacionalizacion.png"} width={300} height={300} alt="kakaka" />
         </div>
       </div> 
     </Layout>
