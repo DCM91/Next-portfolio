08cebe2a3e71c183084d02cf595dc68509062982
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2023-06-01 18:02:38 +0200

    mejorando works

--- pages/works.js
+++ pages/works.js
@@ -4,37 +4,29 @@ import Image from "next/image"
 export default function Works() {
   return (
     <Layout title="Dcm91Portfolio - Works">
-      <div className="flex flex-wrap h-fit px-10 pb-1">
-        <div className="border-solid border-2 border-orange-900 m-1">
+      <div className="flex flex-wrap justify-center h-fit px-10 pb-1">
+        <div className="border-solid border-2 border-orange-900 m-1 max-w-xs transition duration-300 ease-in-out hover:scale-110 ">
           <Image src={"/projects/skuadlack.png"} width={300} height={300} alt="kakaka" />
         </div>
-        <div className="border-solid border-2 border-orange-900 m-1">
-          <Image src={"/projects/blockbuster.png"} width={300} height={300} alt="kakaka" />
+        <div className="border-solid border-2 border-orange-900 m-1 max-w-xs transition duration-300 ease-in-out hover:scale-110">
+          <Image src={"/projects/typed.png"} width={300} height={300} alt="kakaka" />
         </div>
-        <div className="border-solid border-2 border-orange-900 m-1">
-          <Image src={"/projects/pokedex.png"} width={300} height={300} alt="kakaka" />
+        <div className="border-solid border-2 border-orange-900 m-1 max-w-xs transition duration-300 ease-in-out hover:scale-110">
+          <Image src={"/projects/portfolio.png"} width={300} height={300} alt="kakaka" />
         </div>
-        <div className="border-solid border-2 border-orange-900 m-1 flex">
+
+
+
+        <div className="border-solid border-2 border-orange-900 m-1 max-w-xs transition duration-300 ease-in-out hover:scale-110 flex">
           <Image src={"/projects/internacionalizacion.png"} width={300} height={300} alt="kakaka" />
         </div>
-
-
-
-
-
-
-        <div className="border-solid border-2 border-orange-900 m-1">
-          <Image src={"/projects/skuadlack.png"} width={300} height={300} alt="kakaka" />
-        </div>
-        <div className="border-solid border-2 border-orange-900 m-1">
+        <div className="border-solid border-2 border-orange-900 m-1 max-w-xs transition duration-300 ease-in-out hover:scale-110">
           <Image src={"/projects/blockbuster.png"} width={300} height={300} alt="kakaka" />
         </div>
-        <div className="border-solid border-2 border-orange-900 m-1">
+        <div className="border-solid border-2 border-orange-900 m-1 max-w-xs transition duration-300 ease-in-out hover:scale-110">
           <Image src={"/projects/pokedex.png"} width={300} height={300} alt="kakaka" />
         </div>
-        <div className="border-solid border-2 border-orange-900 m-1 flex">
-          <Image src={"/projects/internacionalizacion.png"} width={300} height={300} alt="kakaka" />
-        </div>
+
       </div> 
     </Layout>
   )
