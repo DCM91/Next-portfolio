7e2604074f90c81ed58c7d7e46a553aa30aa4149
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2023-06-12 13:53:09 +0200

    arreglando portada

--- pages/index.js
+++ pages/index.js
@@ -31,15 +31,15 @@ export default function Home() {
 
 
   return (
-      <div className="grid h-screen">
+      <div className="grid">
           <Head>
             <title>Dcm91Portfolio - Home</title>
             <meta name="description" content="Dcm91 profesional Portfolio" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <div className="w-100  m-2 mb-0 pt-4 justify-items-center">
-            <h1 className="text-4xl stat-title my-3 pl-8 flex">{t.home.title} &nbsp; <MdWavingHand/></h1>
+        <div className="w-100 m-2 mb-0 pt-4 justify-items-center">
+            <h1 className="text-4xl stat-title m-5 pl-8 flex">{t.home.title} &nbsp; <MdWavingHand/></h1>
             <p className="text-lg w-full my-2 text-center">
               Ambitious junior professional in &nbsp; 
               <TypeAnimation
@@ -59,9 +59,9 @@ export default function Home() {
             </p>
         </div>
         <div className="flex flex-wrap w-3/4"> 
-          <div className=" flex-1 grid   px-3 m-3">
+          <div className=" flex-1 grid h-80  px-3 m-3">
             <div className="m-1">
-            <div className="inline-flex  items-center">
+            <div className="inline-flex  items-center mb-3">
               <Selector />
               <MdLanguage />
             </div>
@@ -84,7 +84,7 @@ export default function Home() {
           </div>
 
 
-          <div className="flex-2 w-1/4 border-solid border-l-8 border-sucess mb-8 ml-4">
+          <div className="flex-2 border-solid border-l-8 border-sucess w-1/4 mb-8 ml-4">
             <ul className="grid pt-1 ml-4 text-info w-full h-full ">
               <li className="text-lg w-full p-2 flex items-center  transition-transform duration-350 transform hover:translate-x-14 "><Link className="pl-4 w-50" href={"/about"} >{t.navbar.about}</Link></li>
               <li className="text-lg w-full p-2 flex items-center transition-transform duration-350 transform hover:translate-x-14"><Link className="pl-4 w-50" href={"/skills"}>{t.navbar.SKILLS}</Link></li>
@@ -94,7 +94,9 @@ export default function Home() {
 
         </div>
         </div>
-        <Footer />
+        <div className="fixed bottom-0">
+          <Footer  />
+        </div>
       </div>   
   )
 }
